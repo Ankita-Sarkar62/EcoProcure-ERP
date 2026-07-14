fe5296941158b8df8cae55a39291bf25e95ec4b9
Ankita-Sarkar62 <aankitasarkar6288@gmail.com>
2026-07-15 02:43:11 +0530

    Added employee entity and repository

---
+++ ecoprocure/src/main/java/com/ecoprocure/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.ecoprocure.repository;
+import org.springframework.data.jpa.repository.JpaRepository;//JpaRepository includes : save(), findAll(), findById(), delete(), deleteById(), count(), existsById()
+import org.springframework.stereotype.Repository;
+import com.ecoprocure.entity.Employee;
+
+@Repository //tells the springboot that this class is responsible for database operation
+
+
+//JpaRepository<Employee, Integer>: tells that which table we should work with -> Employee..........Integer: datatype of the primary key
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    
+}
